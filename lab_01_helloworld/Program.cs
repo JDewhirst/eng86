a7d09de97011a666e16397c77d42f711cbc5d56f
Jack Dewhirst <jackdewhirst@gmail.com>
2021-04-20 10:48:46 +0100

    added lab_01_helloworld

---
+++ lab_01_helloworld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace lab_01_helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int x = 100;
+            //x += 10;
+            //for (int i = 0; i <= 10; i++)
+            //{
+            //    Console.WriteLine(i);
+            //    x += i;
+            //}
+            Console.WriteLine("Hello, command line demo");
+            foreach (var item in args)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
