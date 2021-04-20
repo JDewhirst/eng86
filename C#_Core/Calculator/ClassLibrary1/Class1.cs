1637102dbd4e73479dcc2f446fe778f761dcad92
Jack Dewhirst <jackdewhirst@gmail.com>
2021-04-20 19:18:03 +0100

    Calculator methods and tests first pass

--- C#_Core/Calculator/ClassLibrary1/Class1.cs
+++ C#_Core/Calculator/ClassLibrary1/Class1.cs
@@ -4,9 +4,29 @@ namespace CalculatorLibrary
 {
     public static class Calculator
     {
-        public static int Add()
+        public static int Add(int x, int y)
         {
+            return x + y;
+        }
 
+        public static int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+
+        public static int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+
+        public static int Divide(int x, int y)
+        {
+            return x / y;
+        }
+
+        public static int Modulus(int x, int y)
+        {
+            return x % y;
         }
     }
 }
