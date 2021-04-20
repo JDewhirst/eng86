b06518c21e008c1bd061d8dcf235539a66ac5059
Jack Dewhirst <jackdewhirst@gmail.com>
2021-04-20 12:05:20 +0100

    added lab_03_operators

---
+++ lab_03_operators/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace lab_03_operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //// Increment Operators
+            //int x = 3, y = 1;
+            //y = x++;
+
+            //int x = 3, y = 1;
+            //y = ++x;
+
+            //// Arithmetic Operators
+            //var sum = 3 + 4;
+
+            //var years = 11;
+            //var dogYears = years * 7;
+
+            //var pounds = 15;
+            //var kilos = pounds / 2.2;
+
+            //var average = (2 + 5 + 1 + 8) / 4;
+
+            //// The division operator
+            //var a = 0.5 / 0.2;
+            //var b = 5 / 2;
+            //var c = 5.0 / 2;
+
+            //// modulus operator
+            //var d = 5 % 2; // returns 1
+
+            //// Excercise: What is 26 days in weeks and days ? 
+            //var x = 26;
+            //var weeks = x / 7;
+            //var days = x % 7;
+            //Console.WriteLine(x + " days is " + weeks + " weeks and " + days + " days");
+
+            // Short circuiting logical operators.
+            //bool isWearingAParachute = false;
+            //bool jumpOutOfPlane = true;
+
+            //if (isWearingAParachute && jumpOutOfPlane)
+            //{
+            //    Console.WriteLine("Geronimo");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Don't jump");
+            //}
+        }
+    }
+}
