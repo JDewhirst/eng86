9953d13dce5a1d2dcf39117bb5f77a32df5ff1a1
Jack Dewhirst <jackdewhirst@gmail.com>
2021-04-20 18:36:35 +0100

    Arrays and Strings 2 Excercise complete

--- Excercises/ArraysAndStringsExercises 2/ArraysAndStringsExercises/ArraysAndStringsExercises/ArraysAndStringsLibs.cs
+++ Excercises/ArraysAndStringsExercises 2/ArraysAndStringsExercises/ArraysAndStringsExercises/ArraysAndStringsLibs.cs
@@ -9,22 +9,35 @@ namespace ArraysAndStringsLib
         // returns a formatted address string given its components
         public static string Address(int number, string street, string city, string postcode)
         {
-            throw new NotImplementedException();
+            return $"{System.Convert.ToString(number)} {street}, {city} {postcode}.";
         }
         // returns a string representing a test score, written as percentage to 1 decimal place
         public static string Scorer(int score, int outOf)
         {
-            throw new NotImplementedException();
+            return $"You got {score} out of {outOf}: {Math.Round( (float)score/(float)outOf * 100, 1) }%";
         }
 
         // returns the double represented by the string, or -999 if conversion is not possible
         public static double ParseNum(string numString)
         {
-            throw new NotImplementedException();
+            try
+            {
+                return System.Convert.ToDouble(numString);
+            }
+            catch (System.FormatException)
+            {
+                return -999;
+            }
         }
         public static string ManipulateString(string input, int num)
         {
-            throw new NotImplementedException();
+            // "  Cathy", 10, "CATHY0123456789"
+            var number = string.Empty;
+            for (int i = 0; i < num; i++)
+            {
+                number = String.Concat(number, i.ToString());
+            }
+            return input.Trim().ToUpper() + number;
         }
     }
 }
