adb823d2a53cf3d0918e8adf2e9505f8e3b61d28
Jack Dewhirst <jackdewhirst@gmail.com>
2021-04-21 14:39:03 +0100

    added lab 07 selection

---
+++ C#_Core/lab_07_selection/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+
+namespace Selection
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //var number = 15;
+            //var result = "";
+
+            //if (number % 5 ==0)
+            //{
+            //    result += "Nish";
+            //} 
+            //else if (number % 5 == 0)
+            //{
+            //    result += "Nash";
+            //}
+
+            //Console.WriteLine("Result");
+
+            //// nested if
+            //var number = 10;
+            //var result = "";
+
+            //if (number % 5 == 0)
+            //{
+            //    result += "Nish";
+            //    if (number % 3 == 0)
+            //    {
+            //        result += "Nash";
+            //    }
+            //    else
+            //    {
+            //        result += "Nosh";
+            //    }
+            //}
+
+            //Console.WriteLine("Result");
+
+            //DaysOfWeek(7);
+            //Console.WriteLine(Priority(2));
+            Console.WriteLine(PassFailTernary(88));
+
+            int mark = 65;
+            int numStars = mark > 60 ? 3 : 1;
+        }
+
+        public static string PassFailTernary(int mark)
+        {
+            return (mark >= 40) ? "Pass" : "Fail";
+
+        }
+
+        public static string Priority(int level)
+        {
+            string priority = "Code ";
+
+            switch (level)
+            {
+                case 3:
+                    priority += "Red";
+                    break;
+                case 2:
+                case 1:
+                    priority += "Amber";
+                    break;
+                case 0:
+                    priority += "Green";
+                    break;
+                default:
+                    priority = "Error";
+                    break;
+            }
+
+            return priority;
+        }
+
+        public static void DaysOfWeek(int day)
+        {
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+                default:
+                    Console.WriteLine("Not Valid");
+                    break;
+            }
+        }
+
+        public static string PassFail(int mark)
+        {
+            var grade = "Fail";
+            if (mark >= 40)
+            {
+                grade = "Pass";
+            }
+            return grade;
+        }
+
+        public static string Grade(int mark)
+        {
+            var grade = "Pass";
+            if (mark >= 40)
+            {
+                if (mark >= 75)
+                {
+                    grade += " with Distinction";
+                }
+                else if (mark >= 60)
+                {
+                    grade += " with Merit";
+                }
+            }
+            else
+            {
+                grade = "Fail";
+            }
+            return grade;
+        }
+    }
+}
---
+++ C#_Core/lab_07_selection_tests/Tests.cs
@@ -0,0 +1,69 @@
+using NUnit.Framework;
+using Selection;
+using System;
+
+namespace lab_07_selection_test
+{
+    public class Tests
+    {
+        [Test]
+        public void Mark40AndOverPasses()
+        {
+            var actual = Program.PassFail(40);
+            Assert.AreEqual("Pass", actual);
+        }
+
+        [Test]
+        public void Mark39AndUnderFails()
+        {
+            var actual = Program.PassFail(39);
+            Assert.AreEqual("Fail", actual);
+        }
+
+        [TestCase(75,"Pass with Distinction")]
+        [TestCase(76, "Pass with Distinction")]
+        [TestCase(100, "Pass with Distinction")]
+        public void Mark75AndOverPassesWithDistinction(int a, string expected)
+        {
+            var actual = Program.Grade(a);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(60,"Pass with Merit")]
+        [TestCase(65, "Pass with Merit")]
+        [TestCase(74,"Pass with Merit")]
+        public void MarkBetween60And74PassesWithMerit(int a, string expected)
+        {
+            var actual = Program.Grade(a);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(101)]
+        [TestCase(-1)]
+        public void MarkOver100ThrowsException(int a)
+        {
+            Assert.Fail();
+            // Assert.Throws<IllegalMarkException>(() => Program.Grade(a);
+        }
+
+        [TestCase(3, "Code Red")]
+        [TestCase(2, "Code Amber")]
+        [TestCase(1, "Code Amber")]
+        [TestCase(0, "Code Green")]
+        [TestCase(5, "Error")]
+        public void PriorityTest(int level, string expected)
+        {
+            var actual = Program.Priority(level);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(60, "Pass")]
+        [TestCase(40, "Pass")]
+        [TestCase(39, "Fail")]
+        public void PassTest(int mark, string expected)
+        {
+            var actual = Program.PassFailTernary(mark);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
