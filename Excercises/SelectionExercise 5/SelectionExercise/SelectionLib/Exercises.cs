c06472fc00b3b1e97d35e0a9be4742dd9b8a129f
Jack Dewhirst <jackdewhirst@gmail.com>
2021-04-21 15:54:01 +0100

    passing all test cases

--- Excercises/SelectionExercise 5/SelectionExercise/SelectionLib/Exercises.cs
+++ Excercises/SelectionExercise 5/SelectionExercise/SelectionLib/Exercises.cs
@@ -7,7 +7,7 @@ namespace SelectionExercisesLib
         // implement this method so that it returns true if num is even, otherwise false
         public static bool EvenOdd(int num)
         {
-            return false;
+            return num % 2 == 0 ;
         }
 
         // returns the type of ticket a customer is eligible for based on their age
@@ -19,7 +19,30 @@ namespace SelectionExercisesLib
         // Throw an ArgumentException if age is less than 0
         public static string TicketType(int age)
         {
-            return string.Empty;
+            if (age < 0) 
+            {
+                throw new ArgumentException("Impossible age, less than 0");
+            }
+            else if ( age < 5 )
+            {
+                return "Free";
+            }
+            else if ( age <= 12 )
+            {
+                return "Child";
+            }
+            else if ( age <= 17)
+            {
+                return "Student";
+            }
+            else if ( age <= 59 )
+            {
+                return "Standard";
+            }
+            else
+            {
+                return "OAP";
+            }
         }
     }
 }
