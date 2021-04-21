8d52b5ba5b384dfef3e78af359950334d6f03154
Jack Dewhirst <jackdewhirst@gmail.com>
2021-04-21 12:28:02 +0100

    added lab 06 iterations

---
+++ C#_Core/lab_06_iterations/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace lab_06_iterations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //for (int i = 0; i <= 10; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //for (int i = 100; i >= 0; i--)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //var myArray = new int[] { 10, 20, 30 };
+            //for (int i = 0; i < myArray.Length; i++)
+            //{
+            //    Console.WriteLine(myArray[i]);
+            //}
+
+            //foreach (var item in myArray)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            // While
+            //int counter = 0;
+            //while (counter < 10)
+            //{
+            //    Console.WriteLine(counter*10);
+            //    counter++;
+            //}
+
+            //int counter = 0;
+            //do
+            //{
+            //    Console.WriteLine(counter * 10);
+            //    counter++;
+            //} while (counter < 10);
+
+            //// Excercise
+            //int sum = 0;
+            //for (int i = 1; i <= 100; i++)
+            //{
+            //    sum += i;
+            //}
+            //Console.WriteLine(sum);
+
+            //string nish = "NISH IS KING";
+
+            //foreach (var character in nish.ToLower())
+            //{
+            //    Console.WriteLine(character);
+            //}
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine(i);
+            //    if (i == 5)
+            //    {
+            //        Console.WriteLine("Breaking out");
+            //        break;               
+            //    }
+            //    Console.WriteLine("in the loop");
+            //}
+
+            //int counter = 0;
+            //while (counter < 10)
+            //{
+            //    counter++;
+            //    Console.WriteLine(counter);
+            //    // if counter is less than 4, restart the loop
+            //    if (counter < 4) continue;
+            //    {
+            //        Console.WriteLine(counter * 10);
+            //    }
+            //}
+
+            //// Exercise
+            //for (int i = 1; i <= 20; i++)
+            //{
+            //    Console.WriteLine(i);
+            //    if ( i % 15 == 0 )
+            //    {
+            //        break;
+            //    }
+            //}
+        }
+    }
+}
