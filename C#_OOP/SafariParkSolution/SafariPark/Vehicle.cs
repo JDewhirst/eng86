6f7e50bbc201e3e66ed5f98f8d4220678bcc9a18
Jack Dewhirst <jackdewhirst@gmail.com>
2021-04-26 18:59:08 +0100

    Vehicle class homework complete

---
+++ C#_OOP/SafariParkSolution/SafariPark/Vehicle.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SafariPark
+{
+    public class Vehicle
+    {
+        private int _capacity = 4;
+        private int _numPassengers;
+        private int _speed;
+
+        public int NumPassengers 
+        {
+            get { return _numPassengers; }
+            set
+            {
+                if (value < 0)
+                {
+                    _numPassengers = 0;
+                }
+                else if (value > _capacity)
+                {
+                    _numPassengers = _capacity;
+                }
+                else
+                {
+                    _numPassengers = value;
+                }
+            }
+        }
+        public int Position { get; private set; }
+
+        public Vehicle()
+        {
+            _speed = 10;
+
+        }
+
+        public Vehicle(int capacity, int speed = 10 )
+        {
+            _speed = speed;
+            _capacity = capacity;
+        }
+
+        public string Move()
+        {
+            Position += _speed;
+            return $"Moving along";
+
+        }
+
+        public string Move(int times)
+        {
+            Position += times * _speed;
+            return $"Moving along {times} times";
+        }
+
+
+
+    }
+}
---
+++ C#_OOP/SafariParkSolution/SsafariParkTests/VehicleTest.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using SafariPark;
+
+namespace SafariParkTests
+{
+    class VehicleTest
+    {
+        [Test]
+        public void WhenADefaultVehicleMovesTwiceItsPositionIs20()
+        {
+            Vehicle v = new Vehicle();
+            var result = v.Move(2);
+            Assert.AreEqual(20, v.Position);
+            Assert.AreEqual("Moving along 2 times", result);
+        }
+
+        [Test]
+        public void WhenAVehicleWithSpeed40IsMovedItsPositionIs40()
+        {
+            Vehicle v = new Vehicle(5, 40);
+            var result = v.Move();
+            Assert.AreEqual(40, v.Position);
+            Assert.AreEqual("Moving along", result);
+        }
+
+        [Test]
+        public void WhenAVehicleRecievesTooManyPassengersItFillsToCapacity()
+        {
+            Vehicle v = new Vehicle();
+            v.NumPassengers = 10;
+            Assert.AreEqual(4, v.NumPassengers);
+        }
+
+        [Test]
+        public void WhenAVehicleRecievesNegativePassengersItFillsTo0()
+        {
+            Vehicle v = new Vehicle();
+            v.NumPassengers = -5;
+            Assert.AreEqual(0, v.NumPassengers);
+        }
+
+    }
+}
