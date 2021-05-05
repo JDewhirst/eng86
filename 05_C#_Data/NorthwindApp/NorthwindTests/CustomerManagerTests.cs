d1bef7bdaeb575ba8994d4f049db44767e5c26b3
Jack Dewhirst <jackdewhirst@gmail.com>
2021-05-05 20:20:58 +0100

    added northwind app

---
+++ 05_C#_Data/NorthwindApp/NorthwindTests/CustomerManagerTests.cs
@@ -0,0 +1,72 @@
+using NUnit.Framework;
+using NorthwindBusiness;
+using NorthwindData;
+using System.Linq;
+
+namespace NorthwindTests
+{
+    public class CustomerTests
+    {
+        CustomerManager _customerManager;
+        [SetUp]
+        public void Setup()
+        {
+           _customerManager = new CustomerManager();
+            // remove test entry in DB if present
+            using (var db = new NorthwindContext())
+            {
+                var selectedCustomers =
+                from c in db.Customers
+                where c.CustomerId == "MANDA"
+                select c;
+
+                db.Customers.RemoveRange(selectedCustomers);
+                db.SaveChanges();
+            }
+        }
+
+        [Test]
+        public void WhenANewCustomerIsAdded_TheNumberOfCustomersIncreasesBy1()
+        {
+            using (var db = new NorthwindContext())
+            {
+
+            }
+        }
+
+        [Test]
+        public void WhenACustomersDetailsAreChanged_TheDatabaseIsUpdated()
+        {
+            using (var db = new NorthwindContext())
+            {
+
+            }
+        }
+
+        [Test]
+
+        public void WhenACustomerIsRemoved_TheyAreNoLongerInTheDatabase()
+        {
+            using (var db = new NorthwindContext())
+            {
+
+            }
+        }
+    
+
+    [TearDown]
+        public void TearDown()
+        {
+            using (var db = new NorthwindContext())
+            {
+                var selectedCustomers =
+                from c in db.Customers
+                where c.CustomerId == "MANDA"
+                select c;
+
+                db.Customers.RemoveRange(selectedCustomers);
+                db.SaveChanges();
+            }
+        }
+    }
+}
