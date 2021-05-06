e40ab743998faf86219ee1bfa202aa9e3a0605e8
Jack Dewhirst <jackdewhirst@gmail.com>
2021-05-06 18:23:59 +0100

    Adds FeudalismContext file

---
+++ 05_C#_Data/FeudalismApp/feudalismApp/feudalismData/FeudalismContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace feudalismData
+{
+    public partial class FeudalismContext : DbContext
+    {
+        public static FeudalismContext Instance { get; } = new FeudalismContext();
+
+        public DbSet<Character> Characters { get; set; }
+        public DbSet<Province> Provinces { get; set; }
+        public DbSet<TerrainDetail> TerrainDetails { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            if (!options.IsConfigured)
+            {
+                options.UseSqlServer(@"Data Source = (localdb)\ProjectsV13; Initial Catalog = Southwind;");
+            }
+        }
+    }
+}
