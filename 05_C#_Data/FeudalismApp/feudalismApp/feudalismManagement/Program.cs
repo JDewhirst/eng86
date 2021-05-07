cf271e1ab539dd2c6c145cdf60d8426ae2413237
Jack Dewhirst <jackdewhirst@gmail.com>
2021-05-07 11:21:17 +0100

    Added terrain details crud

---
+++ 05_C#_Data/FeudalismApp/feudalismApp/feudalismManagement/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using feudalismData;
+
+namespace feudalismManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var _terrainManager = new TerrainManager();
+
+            //_terrainManager.CreateTerrain("Plain", 1);
+            //_terrainManager.CreateTerrain("Forest", 2);
+            //_terrainManager.CreateTerrain("Hill", 2);
+            //_terrainManager.CreateTerrain("Mountain", 3);
+
+            //using (var db = new FeudalismContext())
+            //{
+            //    var terrainQuery = db.TerrainDetails.Where(td => td.TravelSpeed == 2);
+            //    terrainQuery.ToList().ForEach(tq => Console.WriteLine($"Terrain with speed 2 {tq.TerrainDescription}"));
+            //}
+            
+        }
+    }
+}
---
+++ 05_C#_Data/FeudalismApp/feudalismApp/feudalismManagement/TerrainManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using feudalismData;
+
+namespace feudalismManagement
+{
+    public class TerrainManager
+    {
+
+        public TerrainDetail SelectedTerrain { get; set; }
+
+        public void CreateTerrain(string terrainDescription, int travelSpeed)
+        {
+            var newTerrain = new TerrainDetail() { TerrainDescription = terrainDescription, TravelSpeed = travelSpeed };
+            using (var db = new FeudalismContext())
+            {
+                db.TerrainDetails.Add(newTerrain);
+                db.SaveChanges();
+            }
+        }
+
+        public void DeleteTerrain(int terrainId)
+        {
+            using (var db = new FeudalismContext())
+            {
+                SelectedTerrain = db.TerrainDetails.Where(td => td.TerrainDetailId == terrainId).FirstOrDefault();
+                db.TerrainDetails.RemoveRange(SelectedTerrain);
+                db.SaveChanges();
+            }
+        }
+
+        public void UpdateTerrain(int terrainId, string terrainDescription, int travelSpeed)
+        {
+            using (var db = new FeudalismContext())
+            {
+                SelectedTerrain = db.TerrainDetails.Where(td => td.TerrainDetailId == terrainId).FirstOrDefault();
+                SelectedTerrain.TerrainDescription = terrainDescription;
+                SelectedTerrain.TravelSpeed = travelSpeed;
+                db.SaveChanges();
+            }
+        }
+
+        public List<TerrainDetail> ListAllTerrain()
+        {
+            using (var db = new FeudalismContext())
+            {
+                return db.TerrainDetails.ToList();
+            }
+        }
+
+    }
+}
