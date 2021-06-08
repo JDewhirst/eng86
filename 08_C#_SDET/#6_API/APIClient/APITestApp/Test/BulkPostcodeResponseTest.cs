c331855c25d959ae3e28f81ab0442019d936ec0e
Jack Dewhirst <jackdewhirst@gmail.com>
2021-06-08 09:50:04 +0100

    APIClient Bulkpostcode service and tests

--- 08_C#_SDET/#6_API/APIClient/APITestApp/Test/BulkPostcodeResponseTest.cs
+++ 08_C#_SDET/#6_API/APIClient/APITestApp/Test/BulkPostcodeResponseTest.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using Newtonsoft.Json.Linq;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,17 +12,34 @@ namespace APITestApp.Test
     {
         private BulkPostcodeService _bulkPostcodeService;
 
-        //[OneTimeSetUp]
-        //public async Task OneTimeSetUpAsync()
-        //{
-        //    _bulkPostcodeService = new BulkPostcodeService();
-        //    await _bulkPostcodeService.MakeRequestAsync("YO10");
-        //}
+        [OneTimeSetUp]
+        public async Task OneTimeSetUpAsync()
+        {
+            _bulkPostcodeService = new BulkPostcodeService();
+            await _bulkPostcodeService.MakeRequestAsync(new string[] { "OX49 5NU", "M32 0JG", "NE30 1DP" });
+        }
 
-        //[Test]
-        //public void StatusIs200()
-        //{
-        //    Assert.That(_bulkPostcodeService.ResponseContent["status"].ToString(), Is.EqualTo("200"));
-        //}
+        [Test]
+        public void StatusIs200()
+        {
+            Assert.That(_bulkPostcodeService.ResponseContent["status"].ToString(), Is.EqualTo("200"));
+        }
+
+        public void StatusIs200_Alt()
+        {
+            Assert.That(_bulkPostcodeService.StatusCode, Is.EqualTo("200"));
+        }
+        [Test]
+        public void ObjectStatusIs200()
+        {
+            Assert.That(_bulkPostcodeService.ResponseObject.status, Is.EqualTo(200));
+        }
+
+        [Test]
+        public void RegionForOX495NUIsCorrect()
+        {
+            var selectedLocation = _bulkPostcodeService.ResponseObject.result.Where(x => x.query == "OX49 5NU").FirstOrDefault();
+            Assert.That(selectedLocation.result.region, Is.EqualTo("South East"));
+        }
     }
 }
